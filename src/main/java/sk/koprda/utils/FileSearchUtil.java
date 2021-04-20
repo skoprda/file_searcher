8db2c53aa5fba4e45a638938652f62f351e492a6
skoprda <samuel.koprda@outlook.com>
2021-04-20 17:26:31 +0200

    Commented all methods in FileSearchUtil

--- src/main/java/sk/koprda/utils/FileSearchUtil.java
+++ src/main/java/sk/koprda/utils/FileSearchUtil.java
@@ -18,17 +18,49 @@ public class FileSearchUtil {
         // intended to be empty
     }
 
+    /**
+     * Finds all files that are not directories in the file structure starting at root <b>dir</b>ectory
+     *
+     * @param dir pathname to root directory to be traversed
+     * @return List of absolute pathnames of all files that are not directories in the file structure
+     * @throws DirectoryNotFoundException when <b>dir</b> pathname is invalid
+     */
     public static ArrayList<String> findAllfiles(String dir) throws DirectoryNotFoundException {
         return traverseDirFilterSuitable(dir, (file) -> !file.isDirectory());
     }
 
+    /**
+     * Finds directories in the file structure starting at root <b>dir</b>ectory
+     *
+     * @param dir pathname to root directory to be traversed
+     * @return List of absolute pathnames of all directories found in the file structure
+     * @throws DirectoryNotFoundException when <b>dir</b> pathname is invalid
+     */
     public static ArrayList<String> findAllDirectories(String dir) throws DirectoryNotFoundException {
-        return traverseDirFilterSuitable(dir, file -> file.isDirectory());
+        return traverseDirFilterSuitable(dir, File::isDirectory);
     }
+
+    /**
+     * Finds files which pathnames matches a given <b>pattern</b>
+     *
+     * @param pattern pattern to be tested against file names where '?' represents '.' and '*' represents '.*'
+     *                in Java regular expression notation
+     * @param dir pathname to root directory to be traversed
+     * @return List of absolute pathnames of files that names matches a given <b>pattern</b>
+     * @throws DirectoryNotFoundException when <b>dir</b> pathname is invalid
+     */
     public static ArrayList<String> findFilesByPattern(String pattern, String dir) throws DirectoryNotFoundException {
         return traverseDirFilterSuitable(dir, file -> fileMatchesPattern(file, pattern));
     }
 
+    /**
+     *Finds files that were last modified after a given <b>dateTime</b>
+     *
+     * @param dateTime Date to test against files last modification date
+     * @param dir pathname to root directory to be traversed
+     * @return List of absolute pathnames of files that were last modified after a given <b>dateTime</b>
+     * @throws DirectoryNotFoundException when <b>dir</b> pathname is invalid
+     */
     public static ArrayList<String> findFilesByLastChange(LocalDateTime dateTime, String dir)
             throws DirectoryNotFoundException {
 
@@ -41,7 +73,17 @@ public class FileSearchUtil {
         return traverseDirFilterSuitable(dir, isSuitable);
     }
 
-    private static ArrayList<String> traverseDirFilterSuitable(String dir, Predicate<File> isSuitable) throws DirectoryNotFoundException {
+    /**
+     * Traverses file structure starting at the root <b>dir</b>ectory and then applies a <b>isSuitable</b> Predicate
+     * to filter files
+     *
+     * @param dir pathname to root directory to be traversed
+     * @param isSuitable Predicate used to filter files
+     * @return List of absolute pathnames of files that meets the <b>isSuitable</b> predicate
+     * @throws DirectoryNotFoundException when <b>dir</b> pathname is invalid
+     */
+    private static ArrayList<String> traverseDirFilterSuitable(String dir, Predicate<File> isSuitable)
+            throws DirectoryNotFoundException {
         File rootDir = new File(dir);
         if (!rootDir.exists() && !rootDir.isDirectory())
             throw new DirectoryNotFoundException("Given pathname doesn't match any directory!");
@@ -61,6 +103,13 @@ public class FileSearchUtil {
         return suitableFiles;
     }
 
+    /**
+     * Determine whether a <b>file</b>'s pahname matches a <b>patternString</b>
+     * @param file file which name is to be tested against <b>patternString</b>
+     * @param patternString pattern to be tested against file names where '?' represents '.' and '*' represents '.*'
+     *                      in Java regular expression notation
+     * @return true or false whether a given file's pathname matches a given <b>patternString</b>
+     */
     private static boolean fileMatchesPattern(File file, String patternString) {
         patternString = patternString
                 .replace("|", "\\|")
