f4feb4e346953491bb632ce4fd79339b3cab3325
skoprda <samuel.koprda@outlook.com>
2021-04-20 16:46:55 +0200

    Implemented and tested 4/4 methods
    Implemented and tested all 4 methods

--- src/main/java/sk/koprda/utils/FileSearchUtil.java
+++ src/main/java/sk/koprda/utils/FileSearchUtil.java
@@ -2,39 +2,20 @@ package sk.koprda.utils;
 
 import java.io.File;
 import java.time.Instant;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Stack;
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileSearchUtil {
 
     // stateless, thus thread-safe
 
-    private FileSearchUtil(){
+    private FileSearchUtil() {
         // intended to be empty
-    }
-
-    private static ArrayList<String> traverseDirFilterSuitable(String dir, Predicate<File> isSuitable) throws DirectoryNotFoundException {
-        File rootDir = new File(dir);
-        if(!rootDir.exists() && !rootDir.isDirectory())
-            throw new DirectoryNotFoundException("Given pathname doesn't match any directory!");
-        ArrayList<String> suitableFiles = new ArrayList<>();
-        Stack<File> unvisitedDirs = new Stack<>();
-        unvisitedDirs.push(rootDir);
-        File actualDir;
-        while(!unvisitedDirs.isEmpty()){
-            actualDir = unvisitedDirs.pop();
-            for(File actualDirFile: actualDir.listFiles()){
-                if(actualDirFile.isDirectory())
-                    unvisitedDirs.push(actualDirFile);
-                if(isSuitable.test(actualDirFile))
-                    suitableFiles.add(actualDirFile.getAbsolutePath());
-            }
-        }
-        return suitableFiles;
     }
 
     public static ArrayList<String> findAllfiles(String dir) throws DirectoryNotFoundException {
@@ -44,19 +25,53 @@ public class FileSearchUtil {
     public static ArrayList<String> findAllDirectories(String dir) throws DirectoryNotFoundException {
         return traverseDirFilterSuitable(dir, file -> file.isDirectory());
     }
-
-    public static ArrayList<String> findFilesByPattern(String pattern, String dir){
-        return null;
+    public static ArrayList<String> findFilesByPattern(String pattern, String dir) throws DirectoryNotFoundException {
+        return traverseDirFilterSuitable(dir, file -> fileMatchesPattern(file, pattern));
     }
+
     public static ArrayList<String> findFilesByLastChange(LocalDateTime dateTime, String dir)
-            throws DirectoryNotFoundException{
+            throws DirectoryNotFoundException {
 
         Predicate<File> isSuitable = file -> {
             LocalDateTime lastModified = Instant.ofEpochMilli(file.lastModified())
                     .atZone(ZoneId.systemDefault())
                     .toLocalDateTime();
-            return  lastModified.isAfter(dateTime);
+            return lastModified.isAfter(dateTime);
         };
         return traverseDirFilterSuitable(dir, isSuitable);
+    }
+
+    private static ArrayList<String> traverseDirFilterSuitable(String dir, Predicate<File> isSuitable) throws DirectoryNotFoundException {
+        File rootDir = new File(dir);
+        if (!rootDir.exists() && !rootDir.isDirectory())
+            throw new DirectoryNotFoundException("Given pathname doesn't match any directory!");
+        ArrayList<String> suitableFiles = new ArrayList<>();
+        Stack<File> unvisitedDirs = new Stack<>();
+        unvisitedDirs.push(rootDir);
+        File actualDir;
+        while (!unvisitedDirs.isEmpty()) {
+            actualDir = unvisitedDirs.pop();
+            for (File actualDirFile : actualDir.listFiles()) {
+                if (actualDirFile.isDirectory())
+                    unvisitedDirs.push(actualDirFile);
+                if (isSuitable.test(actualDirFile))
+                    suitableFiles.add(actualDirFile.getAbsolutePath());
+            }
+        }
+        return suitableFiles;
+    }
+
+    private static boolean fileMatchesPattern(File file, String patternString) {
+        patternString = patternString
+                .replace("|", "\\|")
+                .replace(".", "\\.")
+                .replace("^", "\\^")
+                .replace("$", "\\$")
+                .replace("?", ".")
+                .replace("*", ".*");
+
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(file.getName());
+        return matcher.matches();
     }
 }
--- src/test/java/sk/koprda/utils/FileSearchUtilTest.java
+++ src/test/java/sk/koprda/utils/FileSearchUtilTest.java
@@ -2,10 +2,12 @@ package sk.koprda.utils;
 
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FileSearchUtilTest {
@@ -13,7 +15,7 @@ public class FileSearchUtilTest {
     String testDir = System.getProperty("user.dir") + "\\src\\test\\resources\\TestDirectory\\";
 
     @Test
-    void testFindAllFiles_1(){
+    void testFindAllFiles_1() {
         List<String> expectedValues = Arrays.asList(
                 testDir + "dir_a\\file_001",
                 testDir + "dir_a\\file_002",
@@ -22,60 +24,60 @@ public class FileSearchUtilTest {
                 testDir + "dir_b\\file_005",
                 testDir + "dir_b\\file_006"
         );
-        try{
+        try {
             List<String> actualValues = FileSearchUtil.findAllfiles("src/test/resources/TestDirectory");
             expectedValues.sort(String::compareTo);
             actualValues.sort(String::compareTo);
             assertEquals(expectedValues, actualValues);
-        }catch(DirectoryNotFoundException dnfe) {
+        } catch (DirectoryNotFoundException dnfe) {
             dnfe.printStackTrace();
         }
     }
 
     @Test
-    void testFindAllFiles_2(){
-        try{
+    void testFindAllFiles_2() {
+        try {
             FileSearchUtil.findAllfiles("non-existing_dir");
-        }catch(DirectoryNotFoundException dnfe){
+        } catch (DirectoryNotFoundException dnfe) {
             assertEquals("Given pathname doesn't match any directory!", dnfe.getMessage());
         }
     }
 
     @Test
-    void testFindAllDirectories_1(){
+    void testFindAllDirectories_1() {
         List<String> expectedValues = Arrays.asList(
                 testDir + "dir_a",
                 testDir + "dir_a\\dir_aa",
                 testDir + "dir_b"
         );
-        try{
+        try {
             List<String> actualValues = FileSearchUtil.findAllDirectories("src/test/resources/TestDirectory");
             expectedValues.sort(String::compareTo);
             actualValues.sort(String::compareTo);
             assertEquals(expectedValues, actualValues);
-        }catch(DirectoryNotFoundException dnfe) {
+        } catch (DirectoryNotFoundException dnfe) {
             dnfe.printStackTrace();
         }
     }
 
     @Test
-    void testFindAllDirectories_2(){
-        try{
+    void testFindAllDirectories_2() {
+        try {
             FileSearchUtil.findAllDirectories("non-existing_dir");
-        }catch(DirectoryNotFoundException dnfe){
+        } catch (DirectoryNotFoundException dnfe) {
             assertEquals("Given pathname doesn't match any directory!", dnfe.getMessage());
         }
     }
 
     @Test
-    void testFindFilesByLastChange_1(){
+    void testFindFilesByLastChange_1() {
         List<String> expectedValues = Arrays.asList(
                 testDir + "dir_a\\file_001",
                 testDir + "dir_a",
                 testDir + "dir_a\\dir_aa\\file_003",
                 testDir + "dir_a\\dir_aa"
         );
-        try{
+        try {
             LocalDateTime dateTime = LocalDateTime.of(
                     2021,
                     4,
@@ -87,25 +89,25 @@ public class FileSearchUtilTest {
             expectedValues.sort(String::compareTo);
             actualValues.sort(String::compareTo);
             assertEquals(expectedValues, actualValues);
-        }catch(DirectoryNotFoundException dnfe) {
+        } catch (DirectoryNotFoundException dnfe) {
             dnfe.printStackTrace();
         }
     }
 
     @Test
-    void testFindFilesByLastChange_2(){
+    void testFindFilesByLastChange_2() {
         List<String> expectedValues = Arrays.asList();
-        try{
+        try {
             LocalDateTime dateTime = LocalDateTime.now();
             List<String> actualValues = FileSearchUtil.findFilesByLastChange(dateTime, "src/test/resources/TestDirectory");
             assertEquals(expectedValues, actualValues);
-        }catch(DirectoryNotFoundException dnfe) {
+        } catch (DirectoryNotFoundException dnfe) {
             dnfe.printStackTrace();
         }
     }
 
     @Test
-    void testFindFilesByLastChange_3(){
+    void testFindFilesByLastChange_3() {
         List<String> expectedValues = Arrays.asList(
                 testDir + "dir_a\\file_001",
                 testDir + "dir_a\\file_002",
@@ -116,8 +118,8 @@ public class FileSearchUtilTest {
                 testDir + "dir_b\\file_005",
                 testDir + "dir_b\\file_006",
                 testDir + "dir_b"
-                );
-        try{
+        );
+        try {
             LocalDateTime dateTime = LocalDateTime.of(
                     2021,
                     4,
@@ -129,8 +131,84 @@ public class FileSearchUtilTest {
             expectedValues.sort(String::compareTo);
             actualValues.sort(String::compareTo);
             assertEquals(expectedValues, actualValues);
-        }catch(DirectoryNotFoundException dnfe) {
+        } catch (DirectoryNotFoundException dnfe) {
             dnfe.printStackTrace();
         }
     }
+
+    @Test
+    void testFindFilesByPattern_1() {
+        try {
+            String pattern = "file?001";
+            List<String> expectedValues = Arrays.asList(testDir + "dir_a\\file_001");
+            List<String> actualValues = FileSearchUtil.findFilesByPattern(pattern, "src/test/resources/TestDirectory");
+            assertEquals(expectedValues, actualValues);
+        } catch (DirectoryNotFoundException dnfe) {
+            dnfe.printStackTrace();
+        }
+    }
+
+    @Test
+    void testFindFilesByPattern_2() {
+        try {
+            String pattern = "d*a";
+            List<String> expectedValues = Arrays.asList(
+                    testDir + "dir_a",
+                    testDir + "dir_a\\dir_aa"
+            );
+            List<String> actualValues = FileSearchUtil.findFilesByPattern(pattern, "src/test/resources/TestDirectory");
+            expectedValues.sort(String::compareTo);
+            actualValues.sort(String::compareTo);
+            assertEquals(expectedValues, actualValues);
+        } catch (DirectoryNotFoundException dnfe) {
+            dnfe.printStackTrace();
+        }
+    }
+
+    @Test
+    void testFindFilesByPattern_3() {
+        try {
+            String pattern = "file_*";
+            List<String> expectedValues = Arrays.asList(
+                    testDir + "dir_a\\file_001",
+                    testDir + "dir_a\\file_002",
+                    testDir + "dir_a\\dir_aa\\file_003",
+                    testDir + "dir_b\\file_004",
+                    testDir + "dir_b\\file_005",
+                    testDir + "dir_b\\file_006"
+            );
+            List<String> actualValues = FileSearchUtil.findFilesByPattern(pattern, "src/test/resources/TestDirectory");
+            expectedValues.sort(String::compareTo);
+            actualValues.sort(String::compareTo);
+            assertEquals(expectedValues, actualValues);
+        } catch (DirectoryNotFoundException dnfe) {
+            dnfe.printStackTrace();
+        }
+    }
+
+    @Test
+    void testFindFilesByPattern_4() {
+        try {
+            String pattern = "*";
+            List<String> expectedValues = Arrays.asList(
+                    testDir + "dir_a\\file_001",
+                    testDir + "dir_a\\file_002",
+                    testDir + "dir_a",
+                    testDir + "dir_a\\dir_aa\\file_003",
+                    testDir + "dir_a\\dir_aa",
+                    testDir + "dir_b\\file_004",
+                    testDir + "dir_b\\file_005",
+                    testDir + "dir_b\\file_006",
+                    testDir + "dir_b"
+            );
+            List<String> actualValues = FileSearchUtil.findFilesByPattern(pattern, "src/test/resources/TestDirectory");
+            expectedValues.sort(String::compareTo);
+            actualValues.sort(String::compareTo);
+            assertEquals(expectedValues, actualValues);
+        } catch (DirectoryNotFoundException dnfe) {
+            dnfe.printStackTrace();
+        }
+    }
+
+
 }
